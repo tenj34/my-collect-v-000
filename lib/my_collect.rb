fb45746d1119189d2fec0cbe6abd7c2eb856b861
Juan Tenezaca <tenj34@gmail.com>
2018-07-14 19:32:18 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(arr)
+    i = 0
+    collect = []
+    while i < arr.length
+      collect << yield(arr[i])
+      i += 1
+    end
+    collect
+end
 
-
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+programs = ['ruby', 'javascript', 'python', 'objective-c']
